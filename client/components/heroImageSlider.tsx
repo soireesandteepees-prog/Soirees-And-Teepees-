80321f2eb8930eefb5e520106fb1765e9b5908f8
Niyi0904 <owoyeminiyi2@gmail.com>
2025-08-13 11:32:19 +0100

    adjusted the home page theme

--- client/components/heroImageSlider.tsx
+++ client/components/heroImageSlider.tsx
@@ -3,10 +3,10 @@
 
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import {image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27} from '../public/assets/images/index';
+import {image1, image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12, image13, image14, image15, image16, image17, image18, image19, image20, image21, image22, image23, image24, image25, image26, image27, image28} from '../public/assets/images/index';
   const images = [
     image13,
-    image12,
+    image28,
     image10,
     image4
   ]
