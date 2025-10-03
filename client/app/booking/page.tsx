e97d9d559a3bce690e889045a1896cbb5c5cefe4
Niyi0904 <owoyeminiyi2@gmail.com>
2025-10-03 21:01:42 +0100

    testing

--- client/app/booking/page.tsx
+++ client/app/booking/page.tsx
@@ -31,7 +31,7 @@ export default function Booking() {
 
 
   const packages = [
-    { id: 'kids-sleepover', name: 'Kids Sleepover', price: 5, duration: '24 hours', guests: 'Minimum of 3 Children' },
+    { id: 'kids-sleepover', name: 'Kids Sleepover', price: 4, duration: '24 hours', guests: 'Minimum of 3 Children' },
     { id: 'adult-sleepover', name: 'Adult Sleepover', price: 465, duration: '24 hours', guests: 'Minimum of 3 Adults' },
     
     { id: 'luxe-indoor-picnics', name: 'Luxe Indoor Picnics', price: 350, duration: '4 hours', guests: 'Minimum of 4 Guests' },
@@ -40,7 +40,7 @@ export default function Booking() {
   ];
 
   const AddOns = [
-    { id: '1', name: '1 to 3 Balloons', price: 5 },
+    { id: '1', name: '1 to 3 Balloons', price: 1 },
     { id: '2', name: '3 to 5 Balloons', price: 200 },
     { id: '3', name: '5 to 10 Balloons', price: 300 },
     { id: '4', name: '10 to 15 Balloons', price: 400 },
