5cb501679b83bb66cea730f3875cc0c2263b2b88
Niyi0904 <owoyeminiyi2@gmail.com>
2025-10-22 01:46:46 +0100

    Working on email5

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // app password or OAuth2 token
+    pass: process.env.EMAIL_PASS,
   },
     tls: { rejectUnauthorized: false },
     pool: true,
